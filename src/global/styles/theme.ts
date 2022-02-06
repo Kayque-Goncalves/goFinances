aa4e0736a20ab868e50041fda3ffc1cc1d9683b6
Kayque Gonçalves <kayque.silva@jbq.global>
2022-02-06 09:46:38 -0300

    feat: create application initial theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,25 @@
+export default {
+  colors: {
+    primary: '#5636D3',
+
+    secondary: '#FF872C',
+    secondaryLight: 'rgba(255, 135, 44, 0.3)',
+
+    success: '#12A454',
+    successLight: 'rgba(18, 164, 64, 0.5)',
+
+    attencion: '#E83F5B',
+    attencionLight: 'rgba(232, 63, 91, 0.5)',
+
+    shape: '#ffffff',
+    title: '#363F5F',
+    text: '#969CB2',
+    background: '#F0F2F5'
+  },
+
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold',
+  }
+}
